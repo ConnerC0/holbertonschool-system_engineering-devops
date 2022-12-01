91d071f4af49eeea51ab0cd6e63168c4e13d5fbb
ConnerC0 <conner.curtis72@gmail.com>
2022-11-30 18:35:13 -0800

    task 3 adjustment

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,25 +7,25 @@ Python script to export data in the JSON format.
 
 import json
 import requests
-from sys import argv
+
 
 if __name__ == '__main__':
-    id = argv[1]
     user = requests.get("https://jsonplaceholder.{}.com/users/{}".
                         format("typicode", id)).json()
 
-    ThingstoDo = requests.get("https://jsonplaceholder.{}.com/todos?userId={}".
-                              format("typicode", id)).json()
-
-    jsonTask = []
-    for task in ThingstoDo:
-        task_dict = {}
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get("completed")
-        task_dict["username"] = user.get("username")
-        jsonTask.append(task_dict)
     json_dict = {}
-    json_dict[id] = jsonTask
+
+    for i in user:
+        ThingstoDo = requests.get("https://jsonplaceholder.{}.com/todos?userId={}".
+                                format("typicode", id)).json()
+        jsonTask = []
+        for task in ThingstoDo:
+            task_dict = {}
+            task_dict["task"] = task.get('title')
+            task_dict["completed"] = task.get("completed")
+            task_dict["username"] = user.get("username")
+            jsonTask.append(task_dict)
+        json_dict[id] = jsonTask
 
     with open("todo_all_employees.json", "w", encoding="UTF8",
               newline='') as file:
